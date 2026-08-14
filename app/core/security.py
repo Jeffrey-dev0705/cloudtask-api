75125cada086a11dcb60f43aa84a9da5552aba50
Jeffery Juan <jheffjuan@gmail.com>
2026-08-14 17:20:12 +0800

    feat(core): introduce security

---
+++ app/core/security.py
@@ -0,0 +1,10 @@
+from datetime import UTC, datetime, timedelta
+from typing import Any
+
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+
+from app.core.config import get_settings
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
